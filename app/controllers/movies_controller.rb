1f2b9f685cf6222a562c6fd696896c2d50db2425
Louis Raymond <louis@louisraymond.com>
2024-11-10 20:46:44 +0000

    Sort by number of stars

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,8 +3,16 @@ class MoviesController < ApplicationController
     @movies = Movie.includes(:director, :actors, :filming_locations, :reviews)
 
     if params[:actor].present?
-      @movies = @movies.joins(:actors).where('actors.name LIKE ?', "%#{params[:actor]}%")
+      @movies = @movies.search_by_actor(params[:actor])
     end
 
+    case params[:sort]
+    when 'stars_asc'
+      @movies = @movies.sort_by_average_stars_asc
+    when 'stars_desc'
+      @movies = @movies.sort_by_average_stars_desc
+    else
+      @movies = @movies.order(created_at: :desc) # Default sorting
+    end
   end
 end
