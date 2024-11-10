37c8e0383b67b5d0df9412c6b8cc32bce981b51d
Louis Raymond <louis@louisraymond.com>
2024-11-10 20:38:08 +0000

    Add scopes for movies to facilitate search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
+# app/models/movie.rb
+
 class Movie < ApplicationRecord
   belongs_to :director
   has_many :movie_actors
@@ -7,4 +9,27 @@ class Movie < ApplicationRecord
   has_many :reviews
 
   validates :title, presence: true
+  validates :year, presence: true
+
+  scope :search_by_actor, ->(actor_name) {
+    joins(:actors).where('actors.name LIKE ?', "%#{actor_name}%")
+  }
+
+  scope :sort_by_average_stars_asc, -> {
+    left_outer_joins(:reviews)
+      .select('movies.*, AVG(reviews.stars) AS average_stars')
+      .group('movies.id')
+      .order('average_stars ASC')
+  }
+
+  scope :sort_by_average_stars_desc, -> {
+    left_outer_joins(:reviews)
+      .select('movies.*, AVG(reviews.stars) AS average_stars')
+      .group('movies.id')
+      .order('average_stars DESC')
+  }
+
+  def average_stars
+    self[:average_stars]
+  end
 end
