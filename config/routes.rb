fcfdbefd8c25e6b608adaf15a312df6e2bb2b7bc
Louis Raymond <louis@louisraymond.com>
2024-11-14 12:21:43 +0000

    cleanup comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # Define RESTful routes for movies, limiting to the index action
   resources :movies, only: [:index]
 
-  # Set the root path of the application to movies#index
   root 'movies#index'
 end
